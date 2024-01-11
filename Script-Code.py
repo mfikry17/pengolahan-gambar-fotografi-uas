8a733e74e3f653af35c7aba9691c0c860e13d394
pacaaaaca <152180147+pacaaaaca@users.noreply.github.com>
2024-01-11 13:27:00 +0700

    UAS PENGOLAHAN GAMBAR

--- Script-Code.py
+++ Script-Code.py
@@ -6,6 +6,21 @@ from rembg import remove
 from PIL import Image
 import cv2
 
+#masukkan kode deteksi tepi
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread("apel.jpg")
+
+edge = cv2.Canny(img,150,150)
+
+plt.subplot(121), plt.imshow(img, cmap="gray")
+plt.title("gambar asli"),plt.xticks([]),plt.yticks([])
+plt.subplot(122),plt.imshow(edge,cmap="gray")
+plt.title("gambar tepi"),plt.xticks([]),plt.yticks([])
+plt.show()
+
 #masukkan file foto yang ingin dihapus background nya
 input_path = '/content/sample_data/pisang.jpg'
 output_path = '/content/sample_data/pisang.png'
